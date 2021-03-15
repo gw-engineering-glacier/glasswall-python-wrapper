49ffc7a72ded19bb399a4667bf7f2d5a688946a2
AngusWR <46464807+AngusWR@users.noreply.github.com>
2021-03-15 16:40:08 +0000

    avoid circular

--- glasswall/__init__.py
+++ glasswall/__init__.py
@@ -5,19 +5,19 @@ import platform
 import tempfile
 from pkg_resources import get_distribution
 
+__version__ = get_distribution(__name__).version
+
+_OPERATING_SYSTEM = platform.system()
+_PYTHON_VERSION = platform.python_version()
+_ROOT = os.path.dirname(__file__)
+_TEMPDIR = os.path.join(tempfile.gettempdir(), "glasswall")
+
 from glasswall import config, content_management, determine_file_type, utils
 from glasswall.libraries.archive_manager.archive_manager import ArchiveManager
 from glasswall.libraries.editor.editor import Editor
 from glasswall.libraries.rebuild.rebuild import Rebuild
 from glasswall.libraries.security_tagging.security_tagging import SecurityTagging
 from glasswall.libraries.word_search.word_search import WordSearch
-
-__version__ = get_distribution(__name__).version
-
-_PYTHON_VERSION = platform.python_version()
-_OPERATING_SYSTEM = platform.system()
-_ROOT = os.path.dirname(__file__)
-_TEMPDIR = os.path.join(tempfile.gettempdir(), "glasswall")
 
 
 class GwReturnObj:
