9c28ecbff7120701b4ee1951acbf4431b68a58ad
AngusWR <46464807+AngusWR@users.noreply.github.com>
2022-12-19 15:12:56 +0000

    Update __init__.py

--- glasswall/__init__.py
+++ glasswall/__init__.py
@@ -5,7 +5,7 @@ import pathlib
 import platform
 import tempfile
 
-__version__ = "0.2.35"
+__version__ = "0.2.36"
 
 _OPERATING_SYSTEM = platform.system()
 _PYTHON_VERSION = platform.python_version()
