a18e45c9a03080db37f9d6b4cd05e8de6131fbc3
Oleksii Yehorov <OYehorov@upwork.glasswallsolutions.com>
2023-05-15 21:21:22 +0300

    Update default sysConfig values.

--- glasswall/content_management/switches/sys.py
+++ glasswall/content_management/switches/sys.py
@@ -17,3 +17,27 @@ class interchange_pretty(Switch):
         self.restrict_values = ["false", "true"]
         self.value = value
         super().__init__(name=self.name, value=self.value, restrict_values=self.restrict_values)
+
+
+class interchange_best_compression(Switch):
+    def __init__(self, value: str):
+        self.name = self.__class__.__name__
+        self.restrict_values = ["false", "true"]
+        self.value = value
+        super().__init__(name=self.name, value=self.value, restrict_values=self.restrict_values)
+
+
+class export_embedded_images(Switch):
+    def __init__(self, value: str):
+        self.name = self.__class__.__name__
+        self.restrict_values = ["false", "true"]
+        self.value = value
+        super().__init__(name=self.name, value=self.value, restrict_values=self.restrict_values)
+
+
+class run_mode(Switch):
+    def __init__(self, value: str):
+        self.name = self.__class__.__name__
+        self.restrict_values = ["editoronly", "enablerebuild"]
+        self.value = value
+        super().__init__(name=self.name, value=self.value, restrict_values=self.restrict_values)
