187a854e96b6738f78305151bdd2ad1caf903397
AngusWR <46464807+AngusWR@users.noreply.github.com>
2023-07-26 15:03:07 +0100

    libdeflate.so.0

--- glasswall/__init__.py
+++ glasswall/__init__.py
@@ -5,7 +5,7 @@ import pathlib
 import platform
 import tempfile
 
-__version__ = "0.2.42"
+__version__ = "0.2.43"
 
 _OPERATING_SYSTEM = platform.system()
 _PYTHON_VERSION = platform.python_version()
