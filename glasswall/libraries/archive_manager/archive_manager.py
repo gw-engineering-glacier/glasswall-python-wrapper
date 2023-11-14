8939837b4f587a0942e68fe2793d8f3d324ad79e
AngusWR <46464807+AngusWR@users.noreply.github.com>
2023-11-14 11:52:27 +0000

    refactor import_archive

--- glasswall/libraries/archive_manager/archive_manager.py
+++ glasswall/libraries/archive_manager/archive_manager.py
@@ -785,7 +785,7 @@ class ArchiveManager(Library):
 
         return exported_archives_dict
 
-    def import_archive(self, input_file: Union[str, bytes, bytearray, io.BytesIO], output_file: Optional[str] = None, output_report: Optional[str] = None, content_management_policy: Union[None, str, bytes, bytearray, io.BytesIO, glasswall.content_management.policies.ArchiveManager] = None, include_analysis_report: Optional[bool] = True, raise_unsupported: Optional[bool] = True):
+    def import_archive(self, input_file: Union[str, bytes, bytearray, io.BytesIO], output_file: Optional[str] = None, output_report: Optional[str] = None, content_management_policy: Union[None, str, bytes, bytearray, io.BytesIO, glasswall.content_management.policies.ArchiveManager] = None, include_analysis_report: Optional[bool] = False, raise_unsupported: Optional[bool] = True):
         """ Imports an archive using the Glasswall engine.
 
         Args:
@@ -793,7 +793,7 @@ class ArchiveManager(Library):
             output_file (Optional[str], optional): Default None. If str, write the archive to the output_file path.
             output_report (Optional[str], optional): Default None. If str, write the analysis report to the output_report path.
             content_management_policy (Union[None, str, bytes, bytearray, io.BytesIO, glasswall.content_management.policies.ArchiveManager], optional): The content management policy to apply.
-            include_analysis_report (Optional[bool], optional): Default True. If True, write the analysis report into the imported archive.
+            include_analysis_report (Optional[bool], optional): Default False. If True, write the analysis report into the imported archive.
             raise_unsupported (bool, optional): Default True. Raise exceptions when Glasswall encounters an error. Fail silently if False.
 
         Returns:
@@ -810,9 +810,11 @@ class ArchiveManager(Library):
             raise TypeError(content_management_policy)
 
         # Convert string path arguments to absolute paths
+        if isinstance(input_file, str):
+            input_file = os.path.abspath(input_file)
+        # Convert string path arguments to absolute paths
         if isinstance(output_file, str):
             output_file = os.path.abspath(output_file)
-
         if isinstance(output_report, str):
             output_report = os.path.abspath(output_report)
 
@@ -835,41 +837,64 @@ class ArchiveManager(Library):
 
         # API function declaration
         self.library.GwFileImportArchive.argtypes = [
-            ct.c_void_p,
-            ct.c_size_t,
-            ct.POINTER(ct.c_void_p),
-            ct.POINTER(ct.c_size_t),
-            ct.POINTER(ct.c_void_p),
-            ct.POINTER(ct.c_size_t),
-            ct.c_char_p,
-            ct.c_int
+            ct.c_void_p,  # void *inputBuffer
+            ct.c_size_t,  # size_t inputBufferLength
+            ct.POINTER(ct.c_void_p),  # void **outputFileBuffer
+            ct.POINTER(ct.c_size_t),  # size_t *outputFileBufferLength
+            ct.POINTER(ct.c_void_p),  # void **outputReportBuffer
+            ct.POINTER(ct.c_size_t),  # size_t *outputReportBufferLength
+            ct.c_char_p,  # const char *xmlConfigString
+            ct.c_int  # int includeAnalysisReports
         ]
 
         # Variable initialisation
-        input_buffer_bytearray = bytearray(input_file_bytes)
-
-        ct_input_buffer = (ct.c_ubyte * len(input_buffer_bytearray)).from_buffer(input_buffer_bytearray)  # void *inputBuffer
-        ct_input_buffer_length = ct.c_size_t(len(input_file_bytes))  # size_t inputBufferLength
-        ct_output_buffer = ct.c_void_p()  # void **outputFileBuffer
-        ct_output_buffer_length = ct.c_size_t()  # size_t *outputFileBufferLength
-        ct_output_report_buffer = ct.c_void_p()  # void **outputReportBuffer
-        ct_output_report_buffer_length = ct.c_size_t()  # size_t *outputReportBufferLength
-        ct_content_management_policy = ct.c_char_p(content_management_policy.encode())  # const char *xmlConfigString
-        ct_include_analysis_report = ct.c_int(int(include_analysis_report))  # int
         gw_return_object = glasswall.GwReturnObj()
+        gw_return_object.input_buffer = ct.create_string_buffer(input_file_bytes)
+        gw_return_object.input_buffer_length = ct.c_size_t(len(input_file_bytes))
+        gw_return_object.output_buffer = ct.c_void_p()
+        gw_return_object.output_buffer_length = ct.c_size_t()
+        gw_return_object.output_report_buffer = ct.c_void_p()
+        gw_return_object.output_report_buffer_length = ct.c_size_t()
+        gw_return_object.content_management_policy = ct.c_char_p(content_management_policy.encode())
+        gw_return_object.include_analysis_report = ct.c_int(int(include_analysis_report))
 
         with utils.CwdHandler(new_cwd=self.library_path):
             # API call
             gw_return_object.status = self.library.GwFileImportArchive(
-                ct.byref(ct_input_buffer),
-                ct_input_buffer_length,
-                ct.byref(ct_output_buffer),
-                ct.byref(ct_output_buffer_length),
-                ct.byref(ct_output_report_buffer),
-                ct.byref(ct_output_report_buffer_length),
-                ct_content_management_policy,
-                ct_include_analysis_report
+                gw_return_object.input_buffer,
+                gw_return_object.input_buffer_length,
+                ct.byref(gw_return_object.output_buffer),
+                ct.byref(gw_return_object.output_buffer_length),
+                ct.byref(gw_return_object.output_report_buffer),
+                ct.byref(gw_return_object.output_report_buffer_length),
+                gw_return_object.content_management_policy,
+                gw_return_object.include_analysis_report
             )
+
+        if gw_return_object.output_buffer and gw_return_object.output_buffer_length:
+            gw_return_object.output_file = utils.buffer_to_bytes(
+                gw_return_object.output_buffer,
+                gw_return_object.output_buffer_length
+            )
+        if gw_return_object.output_report_buffer and gw_return_object.output_report_buffer_length:
+            gw_return_object.output_report = utils.buffer_to_bytes(
+                gw_return_object.output_report_buffer,
+                gw_return_object.output_report_buffer_length
+            )
+
+        # Write output file
+        if hasattr(gw_return_object, "output_file"):
+            if isinstance(output_file, str):
+                os.makedirs(os.path.dirname(output_file), exist_ok=True)
+                with open(output_file, "wb") as f:
+                    f.write(gw_return_object.output_file)
+
+        # Write output report
+        if hasattr(gw_return_object, "output_report"):
+            if isinstance(output_report, str):
+                os.makedirs(os.path.dirname(output_report), exist_ok=True)
+                with open(output_report, "wb") as f:
+                    f.write(gw_return_object.output_report)
 
         input_file_repr = f"{type(input_file)} length {len(input_file)}" if isinstance(input_file, (bytes, bytearray,)) else input_file.__sizeof__() if isinstance(input_file, io.BytesIO) else input_file
         if gw_return_object.status not in successes.success_codes:
@@ -878,29 +903,6 @@ class ArchiveManager(Library):
                 raise errors.error_codes.get(gw_return_object.status, errors.UnknownErrorCode)(gw_return_object.status)
         else:
             log.debug(f"\n\tinput_file: {input_file_repr}\n\toutput_file: {output_file}\n\tstatus: {gw_return_object.status}")
-
-        gw_return_object.output_file = utils.buffer_to_bytes(
-            ct_output_buffer,
-            ct_output_buffer_length
-        )
-        gw_return_object.output_report = utils.buffer_to_bytes(
-            ct_output_report_buffer,
-            ct_output_report_buffer_length
-        )
-
-        # Write output file
-        if gw_return_object.output_file:
-            if isinstance(output_file, str):
-                os.makedirs(os.path.dirname(output_file), exist_ok=True)
-                with open(output_file, "wb") as f:
-                    f.write(gw_return_object.output_file)
-
-        # Write output report
-        if gw_return_object.output_report:
-            if isinstance(output_report, str):
-                os.makedirs(os.path.dirname(output_report), exist_ok=True)
-                with open(output_report, "wb") as f:
-                    f.write(gw_return_object.output_report)
 
         self.release()
 
