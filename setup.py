d30c16976b5b1e4f14ff293e85fac14941df0032
AngusWR <46464807+AngusWR@users.noreply.github.com>
2024-03-01 15:52:37 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@
 import os
 import setuptools
 
-
-with open(os.path.join(os.path.dirname(__file__), "README.md"), "r", encoding="utf-8") as f:
+current_directory = os.path.dirname(os.path.abspath(__file__))
+readme_path = os.path.join(current_directory, "README.md")
+with open(readme_path, "r", encoding="utf-8") as f:
     long_description = f.read()
 
 
@@ -14,8 +15,8 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="AngusWR",
-    author_email="aroberts@glasswallsolutions.com",
-    url="https://github.com/filetrust/glasswall-python",
+    author_email="aroberts@glasswall.com",
+    url="https://github.com/gw-engineering/glasswall-python-wrapper",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.6",
@@ -23,6 +24,8 @@ setuptools.setup(
     python_requires=">=3.6",
     include_package_data=True,
     install_requires=[
-        "lxml"
-    ]
+        "lxml==4.9.4<5",
+        "psutil==5.9.8<6",
+        "tqdm==4.64.1<5",
+    ],
 )
