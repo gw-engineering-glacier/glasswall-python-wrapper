d452633b4ec6ad5691bd94fc9778a28e24a133e7
AngusWR <46464807+AngusWR@users.noreply.github.com>
2024-03-12 17:33:45 +0000

    refactor, fix type warnings

--- glasswall/multiprocessing/memory_usage.py
+++ glasswall/multiprocessing/memory_usage.py
@@ -1,28 +1,31 @@
 
 
 from psutil import Process, NoSuchProcess
+from typing import Optional
 
 
-def get_total_memory_usage_in_gib(pid: int) -> float:
+def get_total_memory_usage_in_gib(pid: Optional[int]) -> float:
     """ Calculate the total memory usage of a process and its child processes in gigibytes (GiB).
 
     Args:
-        pid (int): The process ID for which memory usage is to be calculated.
+        pid (int, optional): The process ID for which memory usage is to be calculated. If None, returns 0.0.
 
     Returns:
-        float: The total memory usage of the process and its children in GiB.
+        float: The total memory usage of the process and its children in GiB. If pid is None, returns 0.0.
     """
-    total_memory: float = 0.0
+    if pid is None:
+        return 0.0
+
     try:
         psutil_process = Process(pid)
-        total_memory = psutil_process.memory_info().rss
-
-        # Include memory usage of child processes
-        for child in psutil_process.children(recursive=True):
-            total_memory += child.memory_info().rss
-
-        return total_memory / 1024 ** 3  # Convert bytes to GiB
-
     except NoSuchProcess:
         # Handle the case where the process does not exist
-        return total_memory
+        return 0.0
+
+    total_memory = psutil_process.memory_info().rss
+
+    # Include memory usage of child processes
+    for child in psutil_process.children(recursive=True):
+        total_memory += child.memory_info().rss
+
+    return total_memory / 1024 ** 3  # Convert bytes to GiB
