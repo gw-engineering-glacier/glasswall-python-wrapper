bb3f88dcade7edcdf7ef1e5735de6a36e6d24d36
AngusWR <46464807+AngusWR@users.noreply.github.com>
2024-03-15 12:57:22 +0000

    rewrap within try

--- glasswall/multiprocessing/memory_usage.py
+++ glasswall/multiprocessing/memory_usage.py
@@ -18,14 +18,14 @@ def get_total_memory_usage_in_gib(pid: Optional[int]) -> float:
 
     try:
         psutil_process = Process(pid)
+        total_memory = psutil_process.memory_info().rss
+
+        # Include memory usage of child processes
+        for child in psutil_process.children(recursive=True):
+            total_memory += child.memory_info().rss
+
+        return total_memory / 1024 ** 3  # Convert bytes to GiB
+
     except NoSuchProcess:
         # Handle the case where the process does not exist
         return 0.0
-
-    total_memory = psutil_process.memory_info().rss
-
-    # Include memory usage of child processes
-    for child in psutil_process.children(recursive=True):
-        total_memory += child.memory_info().rss
-
-    return total_memory / 1024 ** 3  # Convert bytes to GiB
